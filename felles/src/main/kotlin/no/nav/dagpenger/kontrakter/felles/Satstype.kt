c8ad0afb1c3ea659ae479420d9fb1f39c6642bde
Martine Enger <marenger@student.matnat.uio.no>
2024-04-12 10:07:07 +0200

    Kontrakter for utbetaling er flyttet til en ny pakke, kan derfor slettes.

--- felles/src/main/kotlin/no/nav/dagpenger/kontrakter/felles/Satstype.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.dagpenger.kontrakter.felles
-
-@Suppress("unused")
-enum class Satstype {
-    DAGLIG,
-    MÅNEDLIG,
-    ENGANGS,
-}
--- felles/src/main/kotlin/no/nav/dagpenger/kontrakter/felles/StønadType.kt
+++ felles/src/main/kotlin/no/nav/dagpenger/kontrakter/felles/StønadType.kt
@@ -3,16 +3,11 @@ package no.nav.dagpenger.kontrakter.felles
 import com.fasterxml.jackson.annotation.JsonCreator
 
 sealed interface StønadType {
-    fun tilFagsystem(): Fagsystem
-
     companion object {
         @JsonCreator
         @JvmStatic
         fun deserialize(json: String): StønadType? {
-            val stønadstypeDagpenger = StønadTypeDagpenger.values().find { it.name == json }
-            val stønadstypeTiltakspenger = StønadTypeTiltakspenger.values().find { it.name == json }
-            val stønadstypeTilleggsstønader = StønadTypeTilleggsstønader.values().find { it.name == json }
-            return stønadstypeDagpenger ?: stønadstypeTiltakspenger ?: stønadstypeTilleggsstønader
+            return StønadTypeDagpenger.values().find { it.name == json }
         }
     }
 }
@@ -21,39 +16,5 @@ enum class StønadTypeDagpenger : StønadType {
     DAGPENGER_ARBEIDSSOKER_ORDINAER,
     DAGPENGER_PERMITTERING_ORDINAER,
     DAGPENGER_PERMITTERING_FISKEINDUSTRI,
-    DAGPENGER_EOS;
-
-    override fun tilFagsystem(): Fagsystem = Fagsystem.DAGPENGER
-}
-
-enum class StønadTypeTiltakspenger : StønadType {
-    ARBEIDSFORBEREDENDE_TRENING,
-    ARBEIDSRETTET_REHABILITERING,
-    ARBEIDSTRENING,
-    AVKLARING,
-    DIGITAL_JOBBKLUBB,
-    ENKELTPLASS_AMO,
-    ENKELTPLASS_VGS_OG_HØYERE_YRKESFAG,
-    FORSØK_OPPLÆRING_LENGRE_VARIGHET,
-    GRUPPE_AMO,
-    GRUPPE_VGS_OG_HØYERE_YRKESFAG,
-    HØYERE_UTDANNING,
-    INDIVIDUELL_JOBBSTØTTE,
-    INDIVIDUELL_KARRIERESTØTTE_UNG,
-    JOBBKLUBB,
-    OPPFØLGING,
-    UTVIDET_OPPFØLGING_I_NAV,
-    UTVIDET_OPPFØLGING_I_OPPLÆRING,
-    ;
-
-    override fun tilFagsystem(): Fagsystem = Fagsystem.TILTAKSPENGER
-}
-
-enum class StønadTypeTilleggsstønader : StønadType {
-    TILSYN_BARN_ENSLIG_FORSØRGER,
-    TILSYN_BARN_AAP,
-    TILSYN_BARN_ETTERLATTE,
-    ;
-
-    override fun tilFagsystem(): Fagsystem = Fagsystem.TILLEGGSSTØNADER
+    DAGPENGER_EOS,
 }
